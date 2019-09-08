a3a0a8a70c2877dce345e099eabe968b408f5abb
Jeffrey Paulino <jeffreypaulino@gmail.com>
2019-09-08 16:27:17 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,24 +12,12 @@ def monopoly_with_second_tier
   
   countries_and_capitals_of_the_world = {
   :north_america => {
-    "Canada" => {
-      :capital => "Ottawa",
-      :capital_climate => "Kőppen Dfb"
-    },
-    "USA" => {
-      :capital => "Washington D.C.",
-      :capital_climate => "Kőppen Cfa"
-    }
+    "Canada" => {},
+    "USA" => {}
   },
   :africa => {
-    "Ghana" => {
-      :capital => "Accra",
-      :capital_climate => "Kőppen Aw"
-    },
-    "Nigeria" => {
-      :capital => "Abuja",
-      :capital_climate => "Kőppen Aw"
-    }
+    "Ghana" => {},
+    "Nigeria" => {}
   }
 }
   
